e7b2051cfdf5c7f8516fa27c9274dd111cd980a2
Keigh Rim <keigh.rim@gmail.com>
2020-04-10 13:14:23 -0400

    adding multithread support for processing a dir via CLI

--- src/main/java/edu/brandeis/lapps/CliUtil.java
+++ src/main/java/edu/brandeis/lapps/CliUtil.java
@@ -8,17 +8,45 @@ import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class CliUtil {
 
+    public static class ProcessorThread extends Thread {
+
+        BrandeisService service;
+        File inf;
+        Path outf;
+
+        public ProcessorThread(BrandeisService service, File inFname, Path outFname) {
+            this.service = service;
+            this.inf = inFname;
+            this.outf = outFname;
+        }
+        public void run() {
+            try {
+                System.out.println("processing " + this.inf.getName());
+                Files.write(this.outf, processInputStream(service, new FileInputStream(this.inf)).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void processDirectory(BrandeisService tool, Path inDir, String annName) throws IOException {
+        processDirectory(tool, inDir, annName, 8);
+    }
+
+    public static void processDirectory(BrandeisService tool, Path inDir, String annName, int threadpoolsize) throws IOException {
         Path outDir = Paths.get(inDir.toString(),
                 annName + DateTimeFormatter.ofPattern("-yyyyMMdd'T'HHmmssX").withZone(ZoneOffset.UTC).format(Instant.now()));
         Files.createDirectory(outDir);
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(threadpoolsize);
         for (File lifFile : Objects.requireNonNull(inDir.toFile().listFiles((dir, name) -> name.charAt(0) != '.' && name.endsWith(".lif")))) {
-            System.out.println("processing " + lifFile.getName());
             Path outFile = outDir.resolve(Paths.get(lifFile.getName()).getFileName());
-            java.nio.file.Files.write(outFile, processInputStream(tool, new FileInputStream(lifFile)).getBytes());
+            ProcessorThread th = new ProcessorThread(tool, lifFile, outFile);
+            executor.execute(th);
         }
     }
 
